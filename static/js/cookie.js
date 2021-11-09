d0a30018f3073e156219c9338de6fc77c7da3f57
s2ljeun <s2ljeun@gmail.com>
2021-11-09 13:12:49 +0900

    JS | 포춘쿠키 스크롤에 따라 이동 애니메이션

--- static/js/cookie.js
+++ static/js/cookie.js
@@ -1,4 +1,10 @@
 $(document).ready(function ($) {
+    var currentPosition = parseInt($("#cookie-btn").css("top"));
+    $(window).scroll(function () {
+        var position = $(window).scrollTop(); // 현재 스크롤바의 위치값을 반환합니다.
+        $("#cookie-btn").stop().animate({"top": position + currentPosition + "px"}, 700);
+    });
+
     $("#cookie-btn").on("click", function (event) {  //버튼 누르면
         $("#popup-cookie").show();   //팝업 오픈
         $("body").append('<div class="backon"></div>'); //뒷배경
