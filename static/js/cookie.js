8f911e0191c6135431d3d7d292874f5fcca938e0
s2ljeun <s2ljeun@gmail.com>
2021-11-11 15:56:30 +0900

    JS | 쿠키 팝업 내에서 결과 보이도록 하기

--- static/js/cookie.js
+++ static/js/cookie.js
@@ -4,11 +4,18 @@ $(document).ready(function ($) {
         $("body").append('<div class="backon"></div>'); //뒷배경
     });
 
+    $("#get-cookie").on("click", function (event) {  //버튼 누르면
+        $("#popup-cookie").hide(); // 이전 팝업 숨김
+        $("#result-cookie").show();   //결과 팝업 오픈
+    });
+
     $("body").on("click", function (event) {
         if (event.target.className == 'close' || event.target.className == 'backon') {
             console.log("close");
             $("#popup-cookie").hide(); //close버튼이나 뒷배경 클릭시 닫힘
-            $(".backon").hide();
+            $("#result-cookie").hide();
+            $(".backon").remove();
         }
     });
+
 });
